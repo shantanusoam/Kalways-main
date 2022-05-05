030376b5c0f0fbea803170f391d594eb9c25ed5c
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2022-05-05 22:11:54 +0530

    send

--- src/pages/shipFlieghtPage.js
+++ src/pages/shipFlieghtPage.js
@@ -5,29 +5,19 @@ import { shipfreight } from '../components/waystoship/shipfreight';
 import { Autoplay, Pagination, Navigation } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react/swiper-react';
 import Styled from 'styled-components';
+import Bounce from 'react-reveal/Bounce';
 import './shipFleight.css';
 import image1 from '../images/dv01.jpg';
 import image2 from '../images/3t.jpeg';
 import image3 from '../images/Ut02.jpg';
 import image4 from '../images/bt.jpeg';
 import BlockContent from '@sanity/block-content-to-react';
+import {
+  Card,
+  Formcontainer,
+  FormButton,
+} from '../components/shipfreight/shipfreight';
 const urlFor = (source) => builder.image(source);
-const Card = Styled.div`
-
-h1 {
-  div {
-    background: #00e700;
-    width: 10%;
-    transition: all 0.5s ease;
-  }
-  :hover {
-    cursor: pointer;
-    div {
-      width: 100%;
-    }
-  }
-}
-`;
 
 export default function ShipFlightPage() {
   // componentDidMount() {
@@ -51,7 +41,7 @@ export default function ShipFlightPage() {
     <div className="lg:pt-28 flex flex-col pb-10 bg-slate-500">
       <div className="lg:flex lg:flex-row hidden">
         {posts[0]
-          ? posts[0]['content'][1]['rows'][0]['rows'].map((post) => (
+          ? posts[0]['content'][0]['rows'][0]['rows'].map((post) => (
               <img
                 src={urlFor(post.asset._ref)}
                 className="flex-1 h-64 2xl:h-80"
@@ -79,7 +69,7 @@ export default function ShipFlightPage() {
           className="mySwiper"
         >
           {posts[0]
-            ? posts[0]['content'][1]['rows'][0]['rows'].map((post) => (
+            ? posts[0]['content'][0]['rows'][0]['rows'].map((post) => (
                 <SwiperSlide>
                   <img src={urlFor(post.asset._ref)} alt=""></img>
                 </SwiperSlide>
@@ -87,9 +77,21 @@ export default function ShipFlightPage() {
             : null}
         </Swiper>
       </div>
+      <Bounce right>
+        <Formcontainer className="flex flex-col justify-center lg:items-center items-start bg-black p-8">
+          <h3 className="self-center pb-8 pt-4 lg:text-4xl text-2xl text-white">
+            {posts[0] ? posts[0]['content'][1].heading : null}
+          </h3>
+          <FormButton className="self-center">
+            <a href={posts[0] ? posts[0]['content'][1]['ctas'][0].link : null}>
+              {posts[0] ? posts[0]['content'][1]['ctas'][0].title : null}
+            </a>
+          </FormButton>
+        </Formcontainer>
+      </Bounce>
       <Card className=" self-center ">
         <h1 className=" self-center pt-10 text-gray-800 text-3xl 2xl:text-5xl">
-          {posts[0] ? posts[0]['content'][0].heading : null}
+          {posts[0] ? posts[0]['content'][2].heading : null}
           <p className="h-4"></p>
           <div className="w-40 h-2  hover:w-10 transition duration-700 ease-in-out delay-150"></div>
         </h1>
@@ -98,7 +100,7 @@ export default function ShipFlightPage() {
       <div className="flex flex-row lg:pl-16  pl-8 2xl:pt-16 pt-8">
         {posts[0] ? (
           <img
-            src={urlFor(posts[0]['content'][0].image.asset._ref)}
+            src={urlFor(posts[0]['content'][2].image.asset._ref)}
             className="flex-1 h-64 lg:block hidden"
             alt=""
           ></img>
@@ -107,7 +109,7 @@ export default function ShipFlightPage() {
         <div className="self-end 2xl:pl-32 p-2">
           {posts[0] ? (
             <BlockContent
-              blocks={posts[0]['content'][0]['text']}
+              blocks={posts[0]['content'][2]['text']}
               projectId="cjv2tdo2"
               dataset="production"
             />
