9f1dd2004ba20198cadd24a1cfbe164f794ed5d3
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2022-05-18 22:01:43 +0530

    send final redirect changes

--- src/pages/redirect.js
+++ src/pages/redirect.js
@@ -2,9 +2,7 @@ import React, { useEffect } from 'react';
 
 function Redirect() {
   useEffect(() => {
-    if ((this.props.location.state = 'http://localhost:3000/')) {
-      window.location.href = 'https://kalwaycms.sanity.studio/';
-    }
+    window.location.href = 'https://kalwaycms.sanity.studio/';
   }, []);
 
   return (
